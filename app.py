3e070f5321ae60ce9b91a20f18c72409fef33329
AtecPonto <suporteatecponto@gmail.com>
2025-09-19 14:28:17 -0300

    Corrige path do app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Adiciona o path do projeto ao Python
+sys.path.append(os.path.join(os.path.dirname(__file__), 'controle_estoque'))
+
 from controle_estoque.wsgi import application
 
 app = application
